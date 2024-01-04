37a236e7541fdba79d7c0217033084ad5c26d559
thangnt045 <thangnguyen0405@gmail.com>
2024-01-04 07:57:43 +0700

    done workbook 2.2

--- Module 1 - Introduction to Java/2. Variables/Workbook 2.2/Biography.java
+++ Module 1 - Introduction to Java/2. Variables/Workbook 2.2/Biography.java
@@ -1,22 +1,32 @@
 public class Biography {
     public static void main(String[] args) {
-        
-        // Instructions for this workbook are on Learn the Part (see the Udemy Video: Workbook 2.2 to access the link).
 
+        // Instructions for this workbook are on Learn the Part (see the Udemy Video:
+        // Workbook 2.2 to access the link).
 
         // name (text)
         // age (whole number)
         // country (text)
-        // sport (text)
+        // instrument (text)
         // hours (whole number)
         // game (text)
         // subject (text)
         // grade (character)
-     
-        System.out.println("My name is <name>. I'm <age> years old, and I'm from <country> ");
-        System.out.println("My favourite sport is <sport>. I play for <hours> hours a day");
-        System.out.println("When I'm tired, I like to play <game>.");
-        System.out.println("In school, my favourite subject was <subject>, I scored a <grade>.");
+
+        String name = "Kevin";
+        int age = 21;
+        String country = "Viet Nam";
+        String instrument = "Piano";
+        int hours = 1;
+        String game = "duolingo";
+        String subject = "english";
+        char grade = 9;
+
+        System.out.println("My name is " + name + "." + " I'm " + age + " years old, and I'm from " + country + ".");
+        System.out.println(
+                "My favourite instrument is " + instrument + "." + " I play for " + hours + " hours a day" + ".");
+        System.out.println("When I'm tired, I like to play " + game + ".");
+        System.out.println("In school, my favourite subject was " + subject + "," + " I scored a " + grade + ".");
 
         // Compare your result to what's on Learn the Part.
     }
