bced8549a8d9c8ae2d1d928fe7c3387d8e514c65
thangnt045 <thangnguyen0405@gmail.com>
2024-01-04 21:23:53 +0700

    done workbook 3.2

--- Module 1 - Introduction to Java/3. Booleans and Conditionals/Workbook 3.2/JavaMart.java
+++ Module 1 - Introduction to Java/3. Booleans and Conditionals/Workbook 3.2/JavaMart.java
@@ -2,26 +2,36 @@ public class JavaMart {
     public static void main(String[] args) {
         double wallet = 100;
 
-        //See Learn the Part for the complete instructions (link in resources folder of Udemy video).  
+        // See Learn the Part for the complete instructions (link in resources folder of
+        // Udemy video).
 
- 
         double toyCar = 5.99;
         System.out.println("Can I get this car?");
-       
-        //if you have enough money
-            //       print: Sure!
-            //       pay for the toy car 
+        if (wallet >= toyCar) {
+            System.out.println("Sure!");
+        } else {
+            System.out.println("Sorry, I only have " + wallet + " left");
+        }
 
-        //else:  Sorry, I only have <wallet> left.
+        // if you have enough money
+        // print: Sure!
+        // pay for the toy car
 
-        
+        // else: Sorry, I only have <wallet> left.
+        wallet -= toyCar;
         double nike = 95.99;
-        System.out.println("Can I get these nike shoes?");
-        //if you have enough money
-            //       print: Sure!
-            //       pay for the nike shoes 
+        System.out.println("\nI get these nike shoes?");
+        if (wallet >= nike) {
+            System.out.println("Sure!");
+        } else {
+            System.out.println("Sorry, I only have " + wallet + " left");
+        }
 
-        //else: Sorry, I only have <wallet> left.
+        // if you have enough money
+        // print: Sure!
+        // pay for the nike shoes
+
+        // else: Sorry, I only have <wallet> left.
 
     }
 }
