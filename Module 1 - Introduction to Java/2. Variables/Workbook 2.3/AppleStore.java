ce62f05afe70db92cbdf5bbcc9d4948c5466e542
thangnt045 <thangnguyen0405@gmail.com>
2024-01-04 08:28:20 +0700

    done workbook 2.3

--- Module 1 - Introduction to Java/2. Variables/Workbook 2.3/AppleStore.java
+++ Module 1 - Introduction to Java/2. Variables/Workbook 2.3/AppleStore.java
@@ -4,25 +4,36 @@ public class AppleStore {
         int numOfCustomers = 0;
         double profit = 0;
 
-        // Instructions for this workbook are on Learn the Part (see the Udemy Video: Workbook 2.3 to access the link).
+        // Instructions for this workbook are on Learn the Part (see the Udemy Video:
+        // Workbook 2.3 to access the link).
 
         System.out.println("You picked 500 apples from an apple orchard");
-        
+        numOfApples += 500;
+
         System.out.println("Time for business! You're selling each apple for 40 cents");
+        double applePrice = 0.4;
 
         System.out.println("One customer walked in. He bought 4 apples!");
+        numOfApples -= 4;
+        numOfCustomers++;
+        profit = 4 * applePrice;
 
         System.out.println("Another customer walked in. He bought 20 apples!");
+        numOfApples -= 20;
+        numOfCustomers++;
+        profit += (20 * applePrice);
 
         System.out.println("Another customer walked in. She bought 200 apples!");
-        
+        numOfApples -= 200;
+        numOfCustomers++;
+        profit += (200 * applePrice);
 
-        System.out.println("Wow! So far, you made: $<profit>");
-        System.out.println("<number of customers> + customers love your apples.");
-        System.out.println("You have <number of apples> apples left.");
+        System.out.println("Wow! So far, you made: $" + profit);
+        System.out.println(numOfCustomers + " customers love your apples.");
+        System.out.println("You have " + numOfApples + " apples left.");
 
-        // Compare your result to what's on Learn the Part.  
-        
+        // Compare your result to what's on Learn the Part.
+
     }
 
 }
