db09b243f34d9f95da1605eaa47239104e1c1125
thangnt045 <thangnguyen0405@gmail.com>
2024-01-09 23:33:14 +0700

    done workbook 5.5 & 5.6

--- Module 1 - Introduction to Java/5. Loops/Workbook 5.5/CounterTool.java
+++ Module 1 - Introduction to Java/5. Loops/Workbook 5.5/CounterTool.java
@@ -7,8 +7,17 @@ public class CounterTool {
         System.out.println("Jimmy: It depends.");
         System.out.println("Oh, ok...");
 
-       // See detailed instructions on Learn the Part.
-
+        // See detailed instructions on Learn the Part.
+        System.out.print("1. Pick a number to count by: ");
+        int countBy = scan.nextInt();
+        System.out.print("2. Pick a number to start couting from ");
+        int countFrom = scan.nextInt();
+        System.out.print("3. Pick a number to count to: ");
+        int countTo = scan.nextInt();
         scan.close();
+
+        for (int i = countFrom; i <= countTo; i += countBy) {
+            System.out.print(i + "\t");
+        }
     }
 }
--- Module 1 - Introduction to Java/5. Loops/Workbook 5.6/EvenOrOdd.java
+++ Module 1 - Introduction to Java/5. Loops/Workbook 5.6/EvenOrOdd.java
@@ -1,5 +1,14 @@
 public class EvenOrOdd {
     public static void main(String[] args) {
         // See Learn the Part for detailed instructions.
+        for (int i = 0; i <= 19; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i + ": Even\n");
+            } else if (i % 2 == 1) {
+                System.out.println(i + ": Odd\n");
+            } else {
+                System.out.println("Nothing");
+            }
+        }
     }
 }
