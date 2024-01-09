882be69666a530f52ece247ed4db4d31b92c43bb
thangnt045 <thangnguyen0405@gmail.com>
2024-01-09 23:15:09 +0700

    done workbook 5.3 & 5.4

--- Module 1 - Introduction to Java/5. Loops/Workbook 5.3/CountingTool.java
+++ Module 1 - Introduction to Java/5. Loops/Workbook 5.3/CountingTool.java
@@ -5,7 +5,11 @@ public class CountingTool {
         Scanner scan = new Scanner(System.in);
 
         // See Learn the Part for the instructions.
-
+        System.out.print("Hi Tommy, Choose a number to count to: ");
+        int chosenNumber = scan.nextInt();
+        for (int i = 0; i <= chosenNumber; i++) {
+            System.out.print(i + "\t");
+        }
         scan.close();
     }
 }
--- Module 1 - Introduction to Java/5. Loops/Workbook 5.4/Beer.java
+++ Module 1 - Introduction to Java/5. Loops/Workbook 5.4/Beer.java
@@ -1,6 +1,14 @@
 public class Beer {
     public static void main(String[] args) {
-       // See detailed instructions on Learn the Part.
+        // See detailed instructions on Learn the Part.
+        for (int i = 99; i >= 1; i--) {
+            sing(i);
+        }
+    }
+
+    public static void sing(int i) {
+        System.out.println(i + " bottles of beer on the wall, " + i + " bottles of beer!\n" +
+                "take one down, pass it around " + (i - 1) + "," + " bottles of beer on the wall!");
     }
 
 }
