d99553d73d2afec3cef16a9340878f122ebbed77
thangnt045 <thangnguyen0405@gmail.com>
2024-01-09 22:32:52 +0700

    done workbook 5.1 & 5.2

--- Module 1 - Introduction to Java/5. Loops/Workbook 5.1/Megaphone.java
+++ Module 1 - Introduction to Java/5. Loops/Workbook 5.1/Megaphone.java
@@ -1,6 +1,43 @@
 public class Megaphone {
     public static void main(String[] args) {
         // See Learn the Part for the instructions.
+        for (int i = 1; i <= 10; i++) {
+            switch (i) {
+                case 1:
+                    System.out.println("1st time: ");
+                    break;
+                case 2:
+                    System.out.println("2nd time: ");
+                    break;
+                case 3:
+                    System.out.println("3rd time: ");
+                    break;
+                case 4:
+                    System.out.println("4th time: ");
+                    break;
+                case 5:
+                    System.out.println("5th time: ");
+                    break;
+                case 6:
+                    System.out.println("6th time: ");
+                    break;
+                case 7:
+                    System.out.println("7th time: ");
+                    break;
+                case 8:
+                    System.out.println("8th time: ");
+                    break;
+                case 9:
+                    System.out.println("9th time: ");
+                    break;
+                case 10:
+                    System.out.println("10th time: ");
+                    break;
+                default:
+                    break;
+            }
+            System.out.println("If Java was easy, they would call it Python! \n");
+        }
     }
-    
+
 }
--- Module 1 - Introduction to Java/5. Loops/Workbook 5.2/Detention.java
+++ Module 1 - Introduction to Java/5. Loops/Workbook 5.2/Detention.java
@@ -1,8 +1,18 @@
+import java.util.Scanner;
+
 public class Detention {
     public static void main(String[] args) {
 
+        Scanner scan = new Scanner(System.in);
         System.out.println("Hi Bart. I can write lines for you.\nWhat do you want me to write?");
+        String bartStatment = scan.nextLine();
         // See Learn the Part for the instructions.
-        
+        if (bartStatment.equals("I will not forget semi-colons.")) {
+            for (int i = 1; i <= 99; i++) {
+                System.out.println(i + "." + " I will not forget semi-colons.\n");
+            }
+        } else {
+            System.out.println("Good!");
+        }
     }
 }
