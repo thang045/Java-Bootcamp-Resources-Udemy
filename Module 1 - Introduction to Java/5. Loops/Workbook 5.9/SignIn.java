662defc5fdfe910e0d560c10f54b803f5c60d189
thangnt045 <thangnguyen0405@gmail.com>
2024-01-10 10:23:31 +0700

    done workbook 5.9

--- Module 1 - Introduction to Java/5. Loops/Workbook 5.9/SignIn.java
+++ Module 1 - Introduction to Java/5. Loops/Workbook 5.9/SignIn.java
@@ -4,31 +4,42 @@ public class SignIn {
     public static void main(String[] args) {
         String username = "Samantha";
         String password = "Java <3";
-        
-        /* Task 1 
-            1. Pick up a username and password from the user.
-       */
+
+        /*
+         * Task 1
+         * 1. Pick up a username and password from the user.
+         */
         Scanner scan = new Scanner(System.in);
         System.out.println("\nWelcome to Javagram! Sign in below\n");
         System.out.print("- Username: ");
-        //pick up username
+        // pick up username
+        String userName = scan.nextLine();
         System.out.print("- Password: ");
-        //pick up password
+        // pick up password
+        String passWord = scan.nextLine();
 
         // see Learn the Part for the remaining instructions.
-        
-        
-        /* will be useful for task 2
-        
-            System.out.println("\nIncorrect, please try again!\n");
-            System.out.print("- Username: ");
-            usernameEntry = scan.nextLine();
-            System.out.print("- Password: ");
-            passwordEntry = scan.nextLine();
 
-        
-        */
+        /*
+         * will be useful for task 2
+         * 
+         * System.out.println("\nIncorrect, please try again!\n");
+         * System.out.print("- Username: ");
+         * usernameEntry = scan.nextLine();
+         * System.out.print("- Password: ");
+         * passwordEntry = scan.nextLine();
+         * 
+         * 
+         */
+        while ((!username.equals(userName)) || (!password.equals(passWord))) {
+            System.out.println("\nIncorrect, please try again!\n");
+            System.out.print("- Username:");
+            userName = scan.next();
+            System.out.print("\n- Password:");
+            passWord = scan.next();
+        }
+        System.out.println("Sign in successul! Welcome");
         scan.close();
-        
+
     }
 }
