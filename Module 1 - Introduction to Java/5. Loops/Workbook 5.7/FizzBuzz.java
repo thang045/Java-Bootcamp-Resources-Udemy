ff78a99e1d93e17057db97e310f48fd833652b4f
thangnt045 <thangnguyen0405@gmail.com>
2024-01-10 08:50:47 +0700

    done workbook 5.7

--- Module 1 - Introduction to Java/5. Loops/Workbook 5.7/FizzBuzz.java
+++ Module 1 - Introduction to Java/5. Loops/Workbook 5.7/FizzBuzz.java
@@ -2,7 +2,17 @@ public class FizzBuzz {
     public static void main(String[] args) {
 
         // See Learn the Part for detailed instructions.
-
+        for (int i = 0; i < 19; i++) {
+            int result = 0;
+            result += i;
+            if (result % (3 * 5) == 0) {
+                System.out.println(i + ". FizzBuzz\n");
+            } else if (result % 5 == 0) {
+                System.out.println(i + ". Buzz\n");
+            } else if (result % 3 == 0) {
+                System.out.println(i + ". Fizz\n");
+            }
+        }
     }
 
 }
