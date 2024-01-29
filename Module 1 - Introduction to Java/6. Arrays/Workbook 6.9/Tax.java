77ac915c2ffa064a6ba125559af7a69ce8cce96d
thangnt045 <thangnguyen0405@gmail.com>
2024-01-29 15:24:56 +0700

    done workbook 6.9

--- Module 1 - Introduction to Java/6. Arrays/Workbook 6.9/Tax.java
+++ Module 1 - Introduction to Java/6. Arrays/Workbook 6.9/Tax.java
@@ -2,11 +2,17 @@ import java.util.Arrays;
 
 public class Tax {
     public static void main(String[] args) {
-        double[] price = {1.99, 2.99, 3.99, 4.99};
-
+        double[] price = { 1.99, 2.99, 3.99, 4.99 };
+        // taxed price = 100% (original price) + 13% (tax) = 113% => tax = 1.13
+        double tax = 1.13;
+        System.out.println("\nThe original prices are: " + Arrays.toString(price));
+        double[] taxedPrice = new double[4];
+        for (int i = 0; i < price.length; i++) {
+            price[i] *= tax;
+            taxedPrice[i] = price[i];
+        }
         // See instructions on Learn the Part (Workbook 6.9)
-        System.out.println("The original prices are: <original prices>");
-        System.out.println("The prices after tax are: <after tax prices>");
+        System.out.println("\nThe prices after tax are: " + Arrays.toString(taxedPrice));
 
     }
 }
