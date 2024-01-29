355e3c92aaf9d2decc28f66806950c0f1089a5e8
thangnt045 <thangnguyen0405@gmail.com>
2024-01-29 14:47:07 +0700

    done workbook 6.8

--- Module 1 - Introduction to Java/6. Arrays/Workbook 6.8/Receipt.java
+++ Module 1 - Introduction to Java/6. Arrays/Workbook 6.8/Receipt.java
@@ -1,11 +1,19 @@
 public class Receipt {
     public static void main(String[] args) {
 
-
+        String[] apples = { "Gala", "Granny Smith", "Macintosh" };
+        double[] prices = { 1.99, 1.49, 1.29 };
         System.out.println("Here's your receipt:\n");
-        
-        // See instructions on Learn the Part (Workbook 6.8)
-        
-        System.out.println("\t<apple i >: $<price i >"); // to be used in for loop.
+        for (int i = 0; i < apples.length; i++) {
+            for (; i < prices.length; i++) {
+                System.out.println("\tYour " + apples[i] + " apple is $" + prices[i] + "\n");
+            }
+        }
+
+        // Think it simple
+        // for (int i = 0; i < apples.length; i++) {
+        // System.out.println("\t" + apples[i] + ": $" + price[i]);
+        // }
+
     }
 }
