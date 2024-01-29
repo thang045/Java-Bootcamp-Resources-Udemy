cf92bd31930b015808667b555b55e9b159118789
thangnt045 <thangnguyen0405@gmail.com>
2024-01-29 14:16:55 +0700

    done workbook 6.6

--- Module 1 - Introduction to Java/6. Arrays/Workbook 6.6/HighScore.java
+++ Module 1 - Introduction to Java/6. Arrays/Workbook 6.6/HighScore.java
@@ -1,27 +1,29 @@
 public class HighScore {
     public static void main(String[] args) {
-        int[] scores = {randomNumber(), randomNumber(), randomNumber(), randomNumber(), randomNumber(), randomNumber(), randomNumber(), randomNumber(), randomNumber(), randomNumber()};
-        
+        int[] scores = { randomNumber(), randomNumber(), randomNumber(), randomNumber(), randomNumber(), randomNumber(),
+                randomNumber(), randomNumber(), randomNumber(), randomNumber() };
+
         int highScore = 0;
-        System.out.print("Here are the scores: ");    
+        int seat = 0;
+        System.out.print("Here are the scores: ");
         for (int i = 0; i < scores.length; i++) {
             System.out.print(scores[i] + " ");
             if (scores[i] > highScore) {
                 highScore = scores[i];
+                seat = i;
             }
         }
-        
-       
+
         System.out.println("\n\nThe highest score is: " + highScore + ". Impressive!");
-        
+
         // TODO : See instructions on Learn the Part (Workbook 6.6)
-        System.out.println("It's the gentleman in seat: <index>. Give that man a cookie!");
+        System.out.println("\nIt's the gentleman in seat: " + seat + ". Give that man a cookie!");
 
     }
-    
+
     public static int randomNumber() {
         double randomDouble = Math.random() * 50000;
-        int randomInt = (int)randomDouble;
+        int randomInt = (int) randomDouble;
         return randomInt;
     }
 }
