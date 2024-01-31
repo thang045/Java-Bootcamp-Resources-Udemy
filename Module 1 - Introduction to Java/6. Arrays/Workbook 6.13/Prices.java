56bfe8cf2ec48d16ca3d74173beff3b680079818
thangnt045 <thangnguyen0405@gmail.com>
2024-01-31 20:54:04 +0700

    done workbook 6.14

--- Module 1 - Introduction to Java/6. Arrays/Workbook 6.13/Prices.java
+++ Module 1 - Introduction to Java/6. Arrays/Workbook 6.13/Prices.java
@@ -1,34 +1,16 @@
 public class Prices {
     public static void main(String[] args) {
         // The instructions for this workbook are on Learn the Part (workbook 6.13)
-        double[][] prices = {
-                { 12.99, 8.99, 9.99, 10.49, 11.99 },
-                { 0.99, 1.99, 2.49, 1.49, 2.99 },
-                { 8.99, 7.99, 9.49, 9.99, 10.99 }
+        int[][] table = {
+                { 1, 2, 3 },
+                { 4, 5, 6 },
+                { 7, 8, 9 }
         };
 
-        // System.out.println("Baking: " + prices[0][0] + "\t" + prices[0][1] + "\t" +
-        // prices[0][2] +
-        // "\nBeverage: " + prices[1][0] + "\t" + prices[1][1] + "\t" + prices[1][2] +
-        // "\nCereals: " + prices[2][0] + "\t" + prices[2][1] + "\t" + prices[2][2]);
-
-        for (int i = 0; i < prices.length; i++) {
-            switch (i) {
-                case 0:
-                    System.out.println("Baking: ");
-                    break;
-                case 1:
-                    System.out.println("\n\nBeverage: ");
-                    break;
-                case 2:
-                    System.out.println("\n\nCereals: ");
-                    break;
+        for (int i = 0; i < table.length; i++) {
+            for (int j = 0; j < table.length; j++) {
+                System.out.print(table[j][j]);
             }
-            for (int j = 0; j < prices[i].length; j++) {
-                System.out.print(prices[i][j] + " ");
-
-            }
-
         }
     }
 }
--- Module 1 - Introduction to Java/6. Arrays/Workbook 6.14/RandomNumbers.java
+++ Module 1 - Introduction to Java/6. Arrays/Workbook 6.14/RandomNumbers.java
@@ -1,7 +1,33 @@
+import java.util.Arrays;
+
 public class RandomNumbers {
     public static void main(String[] args) {
-        
+
         // The instructions for this workbook are on Learn the Part (Workbook 6.14)
+
+        int[][] array = new int[4][6];
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                array[i][j] = randomNumbers();
+            }
+        }
+
+        print2DArray(array);
+    }
+
+    public static int randomNumbers() {
+        int numbers = ((int) (Math.random() * 100));
+        return numbers;
+    }
+
+    public static void print2DArray(int[][] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("Line " + (i + 1) + ": ");
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println("\n");
+        }
     }
 
 }
